9503a205a063780a3459dd89af99e5f8943cc86a
rubidiumchain <51225157+rubidiumchain@users.noreply.github.com>
2022-05-10 18:51:15 +0900

    Update nftrbdtst1token.test.js

--- nftrbdtst1token.test.js
+++ nftrbdtst1token.test.js
@@ -8,7 +8,7 @@ describe("Nftrbdtst1Token", function () {
     const nftrbdtst1Token = await Nftrbdtst1Token.deploy();
     await nftrbdtst1Token.deployed();
     const nftrbdtst1Token2 = await ethers.getContractAt("Nftrbdtst1Token", nftrbdtst1Token.address);
-    const setValue = await nftrbdtst1Token2.awardItem("0x3BB55ff9eFb99Aa1BDa5E7559B52541dc03A9D91","https://skygallery.net/Nftasset/item-id-8u5h2m.json");
+    const setValue = await nftrbdtst1Token2.awardItem("0x3BB55ff9eFbAaAa1BDa5E7559Aa2541dc03A9D91","https://xxxgallery.net/Nftasset/item-id-8u5h2m.json");
     await setValue.wait();
     console.log('Nftrbdtst1Token id:'+ setValue)
   });
